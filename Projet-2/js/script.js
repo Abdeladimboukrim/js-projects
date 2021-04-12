d9e758f42da3c220a5dbd14425fb8d95cffa0d6d
abdeladimboukrim <boukrimabdeladim@email.com>
2021-04-12 01:06:00 +0000

    projet 4 to do

--- Projet-2/js/script.js
+++ Projet-2/js/script.js
@@ -1,2 +1,10 @@
-//array example const colors = ['black', 'white', 'blue', 'yellow', 'pink', 'green']
+const colors = ['black', 'white', 'blue', 'yellow', 'pink', 'green']; 
+var random_color;
+const button = document.querySelector('button');
+const body = document.querySelector('body');
 
+button.addEventListener('click',() => {     
+         random_color = Math.floor(Math.random() * colors.length);
+         body.style.backgroundColor  = colors[random_color]; 
+console.log(color);
+})
--- projet-1/js/app.js
+++ projet-1/js/app.js
@@ -1,46 +1,10 @@
-// document.getElementById()
-// document.getElementsByClassName()
-// document.getElementsByTagName()
-// document.querySelector()
-// document.querySelectorAll()
-
-
-   
-    function myFunction(e) {
+    function myFunction (e) {
         e.preventDefault();
         var cmt = document.getElementById("message").value;
-        var tar = document.querySelector(".text-uppercase");
-        tar.innerHTML = cmt.value;
-        console.log(tar);
-  }
-
-    document.getElementById("submitBtn").addEventListener("click", myFunction);
+        var target = document.querySelector(".message-content");
+        target.innerText = cmt;
+    
+    }
         
+    document.getElementById("submitBtn").addEventListener("click", myFunction);
 
-// var mnd = document.querySelector('container');
-
-// if(mnd.classList.contains('p-2')){
-
-//     console.log('yes mojood');
-
-// }
-
-
-
-// mnd.name='tapename';
-// mnd.textContent;
-
-// // add books
-// const addForm = forms['#message-form'];
-// addForm.addEventListener('submit', function(e){
-//   e.preventDefault();
-
-//   // create elements
-//   const value = addForm.querySelector('input[type="text"]').value;
-//   const li = document.createElement('li');
-//   const bookName = document.createElement('span');
-//   const deleteBtn = document.createElement('span');
-
-//   // add text content
-//   bookName.textContent = value.h4;
-//   deleteBtn.textContent = 'delete';
