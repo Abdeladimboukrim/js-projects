723b6783b23a7593d64b116024d6863149fbe5d2
abdeladimboukrim <boukrimabdeladim@email.com>
2021-06-18 11:03:12 +0100

    Change Hidden List To Remove List with JavaScript

--- projet-4/app.js
+++ projet-4/app.js
@@ -74,6 +74,7 @@ var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
-    div.style.display = "none";
+    div.remove();
+    updateLS();
   }
 }
